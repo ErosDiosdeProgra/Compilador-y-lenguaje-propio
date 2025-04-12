f92a69b35d1bcc4287756702fcdcd9a82473364f
Eros Cortes <165600812+ErosDiosdeProgra@users.noreply.github.com>
2025-04-12 17:18:31 -0400

    primera versión de calculadora chantita

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*AQUI NOSE QUE HAREMOS pero en el calculadora.y el %left es para indicar la jerarquia en los calculos matemáticos
+    y donde estan los Expr es para decir las expresiones que se van a colocar en el calculo, si lo compilas debieras
+    pones evaluar(numero+numero) o podria ser multiplicacion, divición etc. es una versión super básica pero funcional y
+    puedes poner varios numeros (1+2+4-5*4/2) y weno hay que re mejorarlo y añadir todo lo demás. pero al menos funciona
+    y si vas a probar la calculadora en el cmd dentro de la carpeta donde lo descargues debieras poner
+    bison -dy calculadora.y
+    flex calculadora.l
+    gcc lex.yy.c y.tab.c
+    sino prueba con g++ lex.yy.c y.tab.c
+    si no te funciona F nomas xd
+    */
+    return 0;
+}
